4551e3dcbfa4db2be4ac28cfe23b4d846d2569b9
Charles Loomis <cal@sixsq.com>
2013-11-22 11:30:07 +0100

    update to latest release of apache-libcloud (#2)

--- src/main/python/setup.py
+++ src/main/python/setup.py
@@ -13,7 +13,7 @@ setup(
     packages=['stratuslab', 'stratuslab.libcloud'],
 
     install_requires=[
-        "apache-libcloud ==0.12.4",
+        "apache-libcloud ==0.13.2",
         "stratuslab-client",
         ],
 
